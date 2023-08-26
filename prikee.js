eefd9c26bb639a25178b57841947d57886f16274
cloudmode13 <vigneshkumarait@gmail.com>
2023-08-26 17:57:08 +0530

    new changes

--- prikee.js
+++ prikee.js
@@ -8,8 +8,9 @@ dotenv.config();
 import bcrypt from 'bcrypt'
 import _ from 'lodash'
 import partiesRouter from './routes/partiesRoute.js'
-import createCategoryRouter from './routes/createCategory.js'
+// import createCategoryRouter from './routes/createCategory.js'
 import productRouter from "./routes/product.js"
+import CreateCategory from "./models/CreateCategory.js";
 
 
 const app = express();
@@ -39,9 +40,37 @@ mongoose
   app.use('/parties', partiesRouter)
 
   
-  app.use('/createCategory', createCategoryRouter)
+  // app.use('/createCategory', createCategoryRouter)
 
   app.use('/product', productRouter)
+
+
+  app.post('/create', (req, res) => {
+const {categoryName} = req.body
+
+const data = {categoryName:categoryName}
+
+try {
+  const create = CreateCategory.create(data)
+  if(create)
+   res.status(200).send({msg:'create successfully', data:data})
+  else
+  res.status(400).send({msg:'error'})
+} catch (error) {
+  console.log(error)
+}
+  })
+
+
+  app.get("/create", async (req, res) => {
+    try {
+      const create = await CreateCategory.find({});
+      res.send({ data: create });
+      console.log(1, create);
+    } catch (e) {
+      console.log(e);
+    }
+  });
 
 
 app.get("/", (req, res) => {

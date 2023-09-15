8f03b2c35afa3a9d4d190afb87776e8f021f021e
cloudmode13 <vigneshkumarait@gmail.com>
2023-09-15 17:59:07 +0530

    product post

--- controllers/Item/product.js
+++ controllers/Item/product.js
@@ -1,73 +1,96 @@
-import Product from "../../models/Item/Product.js";
+import Product from '../../models/Item/Product.js';
 
 export async function handleProductPost(req, res) {
-    const { itemName, category, itemCode, itemDescription, customField:[{hsn}], 
-     stockDetails:[{unit,openingStock, date}],pricingDetails:[{salesPrice,purchasePrice,hsnCode,gst,}], } = req.body;
-      
-        const data = {
-            itemName:itemName,
-            category:category,
-            itemCode:itemCode,
-            itemDescription:itemDescription,
-            customField:[{hsn:hsn}],
-            stockDetails:[{unit:unit,
-                openingStock:openingStock,
-                date:date}],
-                pricingDetails:[{salesPrice:salesPrice,purchasePrice:purchasePrice,hsnCode:hsnCode,gst:gst,}] 
-        };
-        
-        try {
-          const productItem = await Product.create(data);
-          console.log(104, productItem);
-          if (productItem) {
-            res.status(201).send({ message: "Party created successfully", data: productItem });
-          } else {
-            res.status(400).send({ message: "Party creation failed" });
-          }
-        } catch (e) {
-          console.log(e);
-        }
+  console.log(req.body);
+  if (!req.body) {
+    res.status(400).send({ message: 'Invalid request body' });
+    return;
   }
+  const {
+    itemName,
+    category,
+    itemCode,
+    itemDescription,
+    customField,
+    // stockDetails: [{ unit, openingStock, date }],
+    // pricingDetails: [{ salesPrice, purchasePrice, gst, hsnCode }],
+  } = req.body;
+  console.log(req.body);
 
+  const data = {
+    itemName,
+    category,
+    itemCode,
+    itemDescription,
+    customField,
+    // stockDetails: [{ unit: unit, openingStock: openingStock, date: date }],
+    // pricingDetails: [
+    //   {
+    //     salesPrice: salesPrice,
+    //     purchasePrice: purchasePrice,
+    //     hsnCode: hsnCode,
+    //     gst: gst,
+    //   },
+    // ],
+  };
 
-  export async function handleProductGet (req, res) {
-    try {
-        const productItem = await Product.find({});
-        res.send({ data: productItem });
-        console.log(1, productItem);
-      } catch (e) {
-        console.log(e);
-      }
+  try {
+    const productItem = await Product.create(data);
+    console.log(104, productItem);
+    if (productItem) {
+      res
+        .status(201)
+        .send({ message: 'Party created successfully', data: productItem });
+    } else {
+      res.status(400).send({ message: 'Party creation failed' });
+    }
+  } catch (e) {
+    console.log(e);
   }
+}
 
-
-  export async function handleProductUpdate (req, res) {
-    try {
-        const updatedProduct = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
-        console.log('hello'+ updatedProduct);
-        if (!updatedProduct) {
-          return res.status(404).send({ message: "Party not found" });
-        }
-    
-        return res.status(200).send({ message: "Successfully updated", data: updatedProduct });
-      } catch (error) {
-        console.error(error);
-        return res.status(500).send({ message: "Server error" });
-      }
+export async function handleProductGet(req, res) {
+  try {
+    const productItem = await Product.find({});
+    res.send({ data: productItem });
+    console.log(1, productItem);
+  } catch (e) {
+    console.log(e);
   }
+}
 
+export async function handleProductUpdate(req, res) {
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true },
+    );
+    console.log('hello' + updatedProduct);
+    if (!updatedProduct) {
+      return res.status(404).send({ message: 'Party not found' });
+    }
 
-  export async function handleProductDelete (req, res) {
-    try {
-        const deletedProduct = await Product.findByIdAndDelete(req.params.id);
-    
-        if (!deletedProduct) {
-          return res.status(404).send({ message: "Party not found" });
-        }
-    
-        return res.status(200).send({ message: "Successfully deleted" });
-      } catch (error) {
-        console.error(error);
-        return res.status(500).send({ message: "Server error" });
-      }
+    return res
+      .status(200)
+      .send({ message: 'Successfully updated', data: updatedProduct });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send({ message: 'Server error' });
   }
+}
+
+export async function handleProductDelete(req, res) {
+  try {
+    const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+
+    if (!deletedProduct) {
+      return res.status(404).send({ message: 'Party not found' });
+    }
+
+    return res.status(200).send({ message: 'Successfully deleted' });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send({ message: 'Server error' });
+  }
+}
--- prikee.js
+++ prikee.js
@@ -1,32 +1,26 @@
-import express from "express";
-import cors from "cors";
+import express from 'express';
+import cors from 'cors';
 import mongoose from 'mongoose';
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 dotenv.config();
-import _ from 'lodash'
-import customerRouter from './routes/Customer/customerModal.js'
-import createCategoryRouter from './routes/Customer/createCategory.js'
-import productRouter from "./routes/Item/product.js"
-import createInvtRouter from "./routes/Item/categoryInventory.js";
-import othersInvtRouter from './routes/Item/othersInventory.js'
-import serviceInvtRouter from './routes/Item/serviceInv.js'
-import createSrvRouter from './routes/Item/SrvCategory.js'
-import othersSrvRouter from './routes/Item/SrvOthers.js'
-
-
-
+import _ from 'lodash';
+import customerRouter from './routes/Customer/customerModal.js';
+import createCategoryRouter from './routes/Customer/createCategory.js';
+import productRouter from './routes/Item/product.js';
+import createInvtRouter from './routes/Item/categoryInventory.js';
+import othersInvtRouter from './routes/Item/othersInventory.js';
+import serviceInvtRouter from './routes/Item/serviceInv.js';
+import createSrvRouter from './routes/Item/SrvCategory.js';
+import othersSrvRouter from './routes/Item/SrvOthers.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-
-
 const port = process.env.PORT || 8000;
 
-
-const mongourl = "mongodb+srv://aruncloudmode:mf2XZuraVUoVEAI6@cluster0.mvermhl.mongodb.net/?retryWrites=true&w=majority"
+const mongourl =
+  'mongodb+srv://aruncloudmode:mf2XZuraVUoVEAI6@cluster0.mvermhl.mongodb.net/?retryWrites=true&w=majority';
 
 mongoose
   .connect(mongourl, {
@@ -34,30 +28,30 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    console.log("DB Connnected");
+    console.log('DB Connnected');
   })
   .catch((e) => console.log(e));
 
-  const db = mongoose.connection; 
+const db = mongoose.connection;
 
-  app.use('/customer', customerRouter)
-  
-  app.use('/createCategory', createCategoryRouter)
+app.use('/customer', customerRouter);
 
-  app.use('/product', productRouter)
+app.use('/createCategory', createCategoryRouter);
 
-  app.use('/categoryInvt', createInvtRouter)
+app.use('/product', productRouter);
 
-  app.use('/othersInvt', othersInvtRouter)
+app.use('/categoryInvt', createInvtRouter);
 
-  app.use('/serviceInvt', serviceInvtRouter)
+app.use('/othersInvt', othersInvtRouter);
 
-  app.use('/categorySrv', createSrvRouter)
+app.use('/serviceInvt', serviceInvtRouter);
 
-  app.use('/othersSrv', othersSrvRouter)
+app.use('/categorySrv', createSrvRouter);
 
-app.get("/", (req, res) => {
-    console.log("hello cloud");
-  });
+app.use('/othersSrv', othersSrvRouter);
+
+app.get('/', (req, res) => {
+  console.log('hello cloud');
+});
 
 app.listen(port, () => console.log(`Listening to localhost: ${port}`));

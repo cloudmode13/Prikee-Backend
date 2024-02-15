5bc498264bbe8b893a35abba964c775b6d64447b
cloudmode13 <vigneshkumarait@gmail.com>
2024-02-15 14:32:44 +0530

    google auth

--- routes/User/auth.route.js
+++ routes/User/auth.route.js
@@ -1,10 +1,16 @@
 import express from 'express';
-import { signin, signup } from '../../controllers/User/auth.controller.js';
+import {
+  google,
+  signin,
+  signup,
+} from '../../controllers/User/auth.controller.js';
 
 const router = express.Router();
 
 router.post('/signup', signup);
 
 router.post('/signin', signin);
+
+router.post('/google', google);
 
 export default router;

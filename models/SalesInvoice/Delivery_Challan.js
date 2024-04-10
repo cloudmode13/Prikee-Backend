01e1cd0ad6f9aa67d1488c8d9e06ccea31cf8632
cloudmode13 <vigneshkumarait@gmail.com>
2024-04-10 13:15:41 +0530

    dch cgst

--- models/SalesInvoice/Delivery_Challan.js
+++ models/SalesInvoice/Delivery_Challan.js
@@ -40,6 +40,8 @@ const deliveryChallanSchema = new mongoose.Schema({
     total: { type: String },
     subTotal: { type: String },
     gst: { type: String },
+    cgst: { type: String },
+    cgstValue: { type: String },
   },
 });
 
